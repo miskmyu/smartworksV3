ea5cb658f29970939effde9672a6eeb64c9d0a28
hsshin <hsshin@hsshin-PC>
2012-04-13 17:47:49 +0900

    subTaskWork create (ing...)

---
+++ src/net/smartworks/server/engine/worklist/model/SubTaskWork.java
@@ -0,0 +1,43 @@
+/*	
+ * $Id$
+ * created by    : hsshin
+ * creation-date : 2012. 4. 13.
+ * =========================================================
+ * Copyright (c) 2012 ManinSoft, Inc. All rights reserved.
+ */
+
+package net.smartworks.server.engine.worklist.model;
+
+public class SubTaskWork extends TaskWork {
+
+	private String commentId;    
+	private String refType;  
+	private String refId;  
+	private String opinion;
+
+	public String getCommentId() {
+		return commentId;
+	}
+	public void setCommentId(String commentId) {
+		this.commentId = commentId;
+	}
+	public String getRefType() {
+		return refType;
+	}
+	public void setRefType(String refType) {
+		this.refType = refType;
+	}
+	public String getRefId() {
+		return refId;
+	}
+	public void setRefId(String refId) {
+		this.refId = refId;
+	}
+	public String getOpinion() {
+		return opinion;
+	}
+	public void setOpinion(String opinion) {
+		this.opinion = opinion;
+	}
+
+}
