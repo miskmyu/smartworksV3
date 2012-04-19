99442f8a5eb1816d5b75a0509a101ca1ef3e7e26
jskim <jskim@maninsoft.co.kr>
2012-04-19 15:30:13 +0900

    css

--- WebContent/sera/js/window_popup.js
+++ WebContent/sera/js/window_popup.js
@@ -1,4 +1,4 @@
 // JavaScript Document
 function TermsPopup() {
-window.open('sera/html/use_personal_terms.htm', 'opWin', 'width=580,height=585,scrollbars=no');
+window.open('sera/html/use_personal_terms.htm', 'opWin', 'width=580,height=581,resizable=no,scrollbars=no');
 }
